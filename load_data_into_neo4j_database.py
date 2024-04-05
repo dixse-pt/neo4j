d73f2917196a756662acd85f5d7857fcbb06b478
Julien <hello@dixse.pt>
2024-04-05 11:55:51 +0200

    Update load_data_into_neo4j_database.py

--- load_data_into_neo4j_database.py
+++ load_data_into_neo4j_database.py
@@ -15,7 +15,7 @@ with driver.session() as session:
     session.run(query)
 
 print('done')
-
+#
 query = '''
 LOAD CSV WITH HEADERS FROM 'https://github.com/pauldechorgnat/cool-datasets/raw/master/ratp/stations.csv' AS row
 CREATE (:Station {
@@ -24,77 +24,7 @@ CREATE (:Station {
     x: toFloat(row.x),
     y: toFloat(row.y),
     Trafic: toInteger(row.Trafic),
-    Ville: row.Ville
+    Ville: row.Ville,
+    ligne: row.ligne
 });
 '''
-
-with driver.session() as session:
-    print(query)
-    session.run(query)
-
-print('done')
-
-#Importer les données des liaisons depuis le fichier CSV
-query = '''
-LOAD CSV WITH HEADERS FROM 'https://github.com/pauldechorgnat/cool-datasets/raw/master/ratp/liaisons.csv' AS row
-MATCH (start:Station {nom_gare: row.start})
-MATCH (stop:Station {nom_gare: row.stop})
-MERGE (start)-[:CONNECTS_TO {ligne: row.ligne}]->(stop);
-'''
-
-with driver.session() as session:
-    print(query)
-    session.run(query)
-
-print('done')
-
-# Créer les relations pour les correspondances entre les lignes
-query = '''
-MATCH (s1:Station), (s2:Station)
-WHERE s1 <> s2
-MATCH (s1)-[:CONNECTS_TO]->(:Station)<-[:CONNECTS_TO]-(s2)
-CREATE (s1)-[:CORRESPONDS_TO]->(s2)
-CREATE (s2)-[:CORRESPONDS_TO]->(s1)
-'''
-
-with driver.session() as session:
-    print(query)
-    session.run(query)
-
-print('done')
-
-
-#Créer les relations pour les liaisons à pied
-query = '''
-MATCH (s1:Station), (s2:Station)
-WHERE s1 <> s2
-WITH s1, s2, distance(
-  point({x: s1.x, y: s1.y}),
-  point({x: s2.x, y: s2.y})
-) AS dist
-WHERE dist < 1000 // Distance inférieure à 1 km
-CREATE (s1)-[:WALKS_TO {distance: dist / 4000}]->(s2);
-'''
-
-with driver.session() as session:
-    print(query)
-    session.run(query)
-
-print('done')
-
-#Créer les relations pour les liaisons par train
-query = '''
-MATCH (start:Station)-[:CONNECTS_TO]->(stop:Station)
-WHERE start <> stop
-WITH start, stop, distance(
-  point({x: start.x, y: start.y}),
-  point({x: stop.x, y: stop.y})
-) AS dist
-CREATE (start)-[:TAKES_TRAIN_TO {time: dist / 40000}]->(stop);
-'''
-
-with driver.session() as session:
-    print(query)
-    session.run(query)
-
-print('done')
